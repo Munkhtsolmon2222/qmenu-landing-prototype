8b98dff2abb13cc37123fc501d68d4984eb4ac94
vpn-batbayara <b170960013@gmail.com>
2025-03-29 09:26:57 +0800

    a

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://nufirhmuq5fi3nnmiod7mqbhuq.appsync-api.ap-east-1.amazonaws.com/graphql', // dev3
+  // baseURL: 'https://nufirhmuq5fi3nnmiod7mqbhuq.appsync-api.ap-east-1.amazonaws.com/graphql', // dev3
   // baseURL: 'https://ckcisswrprhmzgowebu3j6ihmy.appsync-api.ap-east-1.amazonaws.com/graphql', // dev
-  // baseURL: process.env.NEXT_PUBLIC_APP_STAGE || '',
+  baseURL: process.env.NEXT_PUBLIC_APP_STAGE || '',
   headers: {
     'Content-Type': 'application/json',
   },

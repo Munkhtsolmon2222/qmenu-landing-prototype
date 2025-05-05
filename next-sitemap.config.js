faadc69fdbd068367e945582cfa1155f2a6beaff
Munkhtsolmon <monkhtsolmonmuzo@gmail.com>
2025-05-05 18:37:50 +0800

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://qmenu.mn', // Replace with your actual domain
+  generateRobotsTxt: true, // Generate a robots.txt file too
+};
